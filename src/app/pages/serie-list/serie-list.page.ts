445cf4c96fc09e00e7dccad37792391f9886f208
nnino <natalia.nino.l@gmail.com>
2019-10-18 16:21:20 -0300

    Add home view and assets

--- src/app/pages/serie-list/serie-list.page.ts
+++ src/app/pages/serie-list/serie-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-serie-list',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./serie-list.page.scss'],
 })
 export class SerieListPage implements OnInit {
+  
+  public serieId: string;
+  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute) {
 
-  constructor() { }
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const queryParams = await this.activatedRoute.queryParams.pipe(take(1)).toPromise();
+    this.serieId = queryParams.serieId;
+    console.log(this.serieId);
+  }
+
+  goBack(){
+    this.navCtrl.back();
   }
 
 }
